08c86003a618b23d1438ddc97dafa60324e604b3
Eden Esayas <edenesayas1627@gmail.com>
2024-12-01 14:12:26 -0800

    Front end linked to deployed backend

--- client/frontend/src/axiosConfig.js
+++ client/frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-  baseURL: "https://evangadi-forum-12.onrender.com/api",
+  baseURL: "https://evangadi-forum-123.onrender.com",
 });
 
 export default axiosBase;
